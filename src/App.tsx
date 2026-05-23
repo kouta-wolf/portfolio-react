2da740733f556b39cf47630c5c4c91d0a7d6f666
kouta-wolf <koutaookami18@gmail.com>
2026-05-24 07:29:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App () {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4">Hello, World!</h1>
+      <p className="text-gray-700 mb-6">このサイトはポートフォリオサイトの予定です</p>
+    </div>
+  );
+}
