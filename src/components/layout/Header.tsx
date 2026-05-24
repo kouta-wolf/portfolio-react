1d42ec955d2e4840a3078fafffab8c7c75de7968
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 08:21:10 +0900

    ヘッダーフッターの色付け #6

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full h-16 bg-blue-500 text-white flex items-center justify-between">
+    <header className="fixed top-0 left-0 w-full h-16 bg-primary text-white flex items-center justify-between">
       <div className="title">ポートフォリオ</div>
       <div className="link">
         <a href="#">About</a>
