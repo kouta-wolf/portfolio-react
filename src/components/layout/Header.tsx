c56141814c86c29b6e06b3d59412c66e78b18a49
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 08:29:48 +0900

    ヘッダーフッターのスタイリング #7

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,13 @@
 export const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-16 bg-primary text-white flex items-center justify-between">
-      <div className="title">ポートフォリオ</div>
-      <div className="link">
-        <a href="#">About</a>
-        <a href="#">Portfolio</a>
-        <a href="#">Social</a>
+      <div className="m-6">
+        <a href="#home" className="sm:text-sm lg:text-2xl font-bold">ポートフォリオ</a>
+      </div>
+      <div className="hidden lg:flex m-6 space-x-6 ">
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#social">Social</a>
       </div>
     </header>
   );
