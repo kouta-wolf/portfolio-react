9d441ffde30738063e0f245e629225bc2995d90e
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 14:09:23 +0900

    refactor: 変数名をわかり易い名前に変更

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
+import { projects } from "../../data/projects.ts"
 
 export const Portfolio = () => {
-  const [content, setContent] = useState(1);
+  const [selectedId, setSelectedId] = useState(1);
+  const item = projects.find((project) => project.id === selectedId);
 
   return (
     <section className="bg-blue-50 min-h-[50vh] flex flex-col items-center justify-center gap-6 px-4 text-center">
       <h1 className="text-3xl font-bold text-center">Portfolio セクション</h1>
       <div className="flex flex-row items-center justify-center gap-6">
         <div className="flex flex-col">
-          <button onClick={() => setContent(1)}>1を表示</button>
-          <button onClick={() => setContent(2)}>2を表示</button>
-          <button onClick={() => setContent(3)}>3を表示</button>
+          {projects.map((item) => (
+            <button key={item.id} onClick={() => setSelectedId(item.id)}>
+              {item.title}
+            </button>
+          ))}
         </div>
         <div>
-          {content === 1 && <p>1が表示</p>}
-          {content === 2 && <p>2が表示</p>}
-          {content === 3 && <p>3が表示</p>}
+          {selectedId && <p>{item?.desc}</p>}
         </div>
       </div>
     </section>
