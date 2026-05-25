2811608f2e3aeec04dfaf6ecb666df016f7c1a8b
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 09:32:52 +0900

    Aboutセクションを仮作成 #5

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,11 @@
 export const About = () => {
   return (
-    <section className="h-screen bg-gray-500 items-center justify-center">
-      <h1 className="text-3xl text-white font-bold text-center">About セクション</h1>
+    <section id="about" className="bg-white min-h-[50vh] flex flex-row-reverse items-center justify-center gap-6 px-4 text-center">
+      <div className="textsection">
+        <h1 className="text-3xl font-bold text-center">About</h1>
+        <p>プログラミングに注力してそろそろ半年の開発者です。<br></br>現在はWebページに関わる様々な言語を<br></br>全体的に学んでいる最中です。</p>
+      </div>
+      画像テスト
     </section>
   );
 };
