47f0ef4953d5fbb9eb660181bf0d3e356c9a5687
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 13:51:00 +0900

    feat: ボタンを押すと表示が切り替わるポートフォリオの例を実装

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const Portfolio = () => {
+  const [content, setContent] = useState(1);
+
   return (
-    <section className="h-screen bg-amber-500 items-center justify-center">
-      <h1 className="text-3xl text-white font-bold text-center">Portfolio セクション</h1>
+    <section className="bg-blue-50 min-h-[50vh] flex flex-col items-center justify-center gap-6 px-4 text-center">
+      <h1 className="text-3xl font-bold text-center">Portfolio セクション</h1>
+      <div className="flex flex-row items-center justify-center gap-6">
+        <div className="flex flex-col">
+          <button onClick={() => setContent(1)}>1を表示</button>
+          <button onClick={() => setContent(2)}>2を表示</button>
+          <button onClick={() => setContent(3)}>3を表示</button>
+        </div>
+        <div>
+          {content === 1 && <p>1が表示</p>}
+          {content === 2 && <p>2が表示</p>}
+          {content === 3 && <p>3が表示</p>}
+        </div>
+      </div>
     </section>
   );
 };
